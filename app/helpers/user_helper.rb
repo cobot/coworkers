a5379262907ec2500ff3791c7f708cffcda1d0d8
Matthias Jakel <matthiasjakel@gmail.com>
2011-06-07 17:00:14 +0200

    user image url helper now detects https, added skype_url helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,13 +2,17 @@ require 'digest/md5'
 
 module UserHelper
   
-  def user_image_url(user, size = 50, secure = false)
+  def user_image_url(user, size = 50, secure = request.ssl?)
     md5 = Digest::MD5.hexdigest(user.email)
     if secure
       "https://secure.gravatar.com/avatar/#{md5}?d=mm&size=#{size}"
     else
       "http://gravatar.com/avatar/#{md5}?d=mm&size=#{size}"
     end
+  end
+  
+  def skype_url(user)
+    "skype:#{user.messenger_account}?add"
   end
   
 end
