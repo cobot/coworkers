2108b75de7a7c593e18efc62f9bebcb558680201
Matthias Jakel <matthiasjakel@gmail.com>
2011-06-09 12:02:40 +0200

    fixed user image helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,7 +3,10 @@ require 'digest/md5'
 module UserHelper
   
   def user_image_url(user, size = 50, secure = request.ssl?)
-    md5 = Digest::MD5.hexdigest(user.email)
+    email = user.email
+    if user.email.nil?
+      email = "info@cobot.me"
+    md5 = Digest::MD5.hexdigest(email)
     if secure
       "https://secure.gravatar.com/avatar/#{md5}?d=mm&size=#{size}"
     else
