e73eaa1126b08817e195fd9a50dc1c226c397b84
Matthias Jakel <matthiasjakel@gmail.com>
2011-06-20 12:19:59 +0200

    added json api for memberships and spaces with only: show and jsonp support

---
+++ app/controllers/api/memberships_controller.rb
@@ -0,0 +1,41 @@
+module Api
+  class MembershipsController < ApplicationController
+    skip_before_filter :require_authentication, only: :show
+  
+    def show
+      @space = db.load params[:space_id]
+      @membership = db.load params[:id]
+      unless params[:callback].blank?
+        render js: "#{params[:callback]}('#{membership_hash(@membership).to_json}');"
+      else
+        render json: membership_hash(@membership)
+      end
+    end
+    
+    def membership_hash(membership)
+      {
+        id: membership._id,
+        name: membership.name,
+        image_url: user_image_url(membership.user),
+        login: membership.user.login,
+        website: membership.user.website,
+        bio: membership.user.bio,
+        profession: membership.user.profession,
+        industry: membership.user.industry,
+        skills: membership.user.skills
+      }
+    end
+    
+    def user_image_url(user, size = 50, secure = request.ssl?)
+      email = "info@cobot.me"
+      email = user.email unless user.email.nil?
+
+      md5 = Digest::MD5.hexdigest(email)
+      if secure
+        "https://secure.gravatar.com/avatar/#{md5}?d=mm&size=#{size}"
+      else
+        "http://gravatar.com/avatar/#{md5}?d=mm&size=#{size}"
+      end
+    end
+  end
+end
---
+++ app/controllers/api/spaces_controller.rb
@@ -0,0 +1,53 @@
+require 'digest/md5'
+
+module Api
+  class SpacesController < ApplicationController
+    skip_before_filter :require_authentication, only: :show
+
+    def show
+      @space = db.load! params[:id]
+      unless params[:callback].blank?
+        render js: "#{params[:callback]}('#{space_hash(@space).to_json}');"
+      else
+        render json: space_hash(@space)
+      end
+    end
+    
+    def space_hash(space)
+      {
+        name: space.name,
+        id: space._id,
+        url: url_for(space),
+        memberships: space.memberships.map {|membership| membership_hash(space, membership)}
+      }
+    end
+    
+    def membership_hash(space, membership)
+      {
+        id: membership._id,
+        name: membership.name,
+        url: url_for([space, membership]),
+        image_url: user_image_url(membership.user),
+        login: membership.user.login,
+        website: membership.user.website,
+        bio: membership.user.bio,
+        profession: membership.user.profession,
+        industry: membership.user.industry,
+        skills: membership.user.skills
+      }
+    end
+    
+    def user_image_url(user, size = 50, secure = request.ssl?)
+      email = "info@cobot.me"
+      email = user.email unless user.email.nil?
+
+      md5 = Digest::MD5.hexdigest(email)
+      if secure
+        "https://secure.gravatar.com/avatar/#{md5}?d=mm&size=#{size}"
+      else
+        "http://gravatar.com/avatar/#{md5}?d=mm&size=#{size}"
+      end
+    end
+    
+  end
+end
