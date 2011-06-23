4531994aa15ffc4ab27eda4abebde0a4dc154745
Matthias Jakel <matthiasjakel@gmail.com>
2011-06-23 10:30:36 +0200

    pagination in coworkers widget

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -1,15 +1,31 @@
+var list
+
 function coworkersWidget(space) {
-  var space = JSON.parse(space);
-  var title = document.createElement('li');
-  title.id = 'cobot_title';
-  title.innerHTML = '<a href="'+space.url+'" title="">'+space.name+'</a>';
-  var cobot_widget = document.getElementById('cobot_widget').appendChild(title);
-  for(i in space.memberships) {
-    var membership = space.memberships[i];
-    var item = document.createElement('li');
-    item.innerHTML = '<img src="'+membership.image_url+'&size=24" alt="" /><a href="'+membership.url+'">'+membership.name+'</a>';
-		if(membership.profession != null)
-		  item.innerHTML += '<span>'+membership.profession+'</span>';
-    document.getElementById('cobot_widget').appendChild(item);
-  }
+  list = JSON.parse(space).memberships;
+  showPage(1, 20);
 };
+
+function showPage(page, items) {
+	page = page || 1;
+	items = items || 20;
+	
+	var cobot_widget = document.getElementById('cobot_widget');
+	
+	cobot_widget.innerHTML = null;
+	
+	var content = "";
+	
+	for(i=(page-1)*items; i<page*items && i<list.length; i++) {	
+	  content += '<li><img src="'+list[i].image_url+'&size=24" alt="" /><a href="'+list[i].url+'">'+list[i].name+'</a>'; 	  
+    if(list[i].profession)
+	    content += '<span>'+list[i].profession+'</span>';
+	  content += '</li>';
+	}
+		
+	var pages = Math.ceil(list.length / 20);
+	for(i=1; i<=pages; i++) {
+    content += '<a href="#" onclick="showPage('+i+')">'+i+'</a>';
+  }
+
+  cobot_widget.innerHTML = content;
+}
