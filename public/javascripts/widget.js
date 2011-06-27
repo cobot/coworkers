f1484d33850586b421176b11d9b896fdad006ee7
Matthias Jakel <matthiasjakel@gmail.com>
2011-06-27 16:35:53 +0200

    added embed code for coworkers widget to space page

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -9,27 +9,25 @@ function showPage(page, items) {
 	page = page || 1;
 	items = items || 10;
 	
-	var cobot_widget = document.getElementById('cobot_widget');
-	
-	cobot_widget.innerHTML = null;
-	
+	var coworkers_widget = document.getElementById('coworkers_widget');
+	coworkers_widget.innerHTML = null;
 	var content = "";
-	
 	content += '<ul>';
+	
 	for(i=(page-1)*items; i<page*items && i<list.length; i++) {	
 	  content += '<li><img src="'+list[i].image_url+'&size=24" alt="" /><a href="'+list[i].url+'">'+list[i].name+'</a>'; 	  
     if(list[i].profession)
 	    content += '<span>'+list[i].profession+'</span>';
 	  content += '</li>';
 	}
+	
 	content += '</ul>';
-		
 	var pages = Math.ceil(list.length / items);
 	content += '<div class="pagination">';
+	
 	for(i=1; i<=pages; i++) {
     content += '<a href="#" onclick="showPage('+i+')">'+i+'</a>';
   }
   content += '</div>';
-
-  cobot_widget.innerHTML = content;
+  coworkers_widget.innerHTML = content;
 }
