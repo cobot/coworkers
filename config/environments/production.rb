443c19b58e224f46d2c090c8d2bacb28d3767de1
Alexander Lang <alex@upstre.am>
2012-12-21 22:17:30 +0100

    enables compiling assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,12 +30,7 @@ Coworkers::Application.configure do
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
-
-  config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
   config.assets.compress = true
-  config.assets.compile = false
-  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
