3bcd06d0522a28b0eb1a2715f58688535e380e5a
Thilo Utke <thilo@upstre.am>
2013-02-26 00:23:07 +0100

    try to debug omniauth issues

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cobot, Coworkers::Conf.app_id,
-    Coworkers::Conf.app_secret, scope: 'read navigation',
+    Coworkers::Conf.app_secret, scope: 'navigation read',
       client_options: {site: Coworkers::Conf.app_site,
         authorize_url: "#{Coworkers::Conf.site}/oauth2/authorize",
         token_url: "#{Coworkers::Conf.site}/oauth2/access_token"}
