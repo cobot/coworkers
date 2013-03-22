66f169c795adf8dd9994126e8c80816c6fc70c77
Alexander Lang <alex@upstre.am>
2013-03-22 15:45:25 +0100

    removes outdated css_compressor key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,6 @@ Coworkers::Application.configure do
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
   config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
   config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
