36c399567524dbc53f131c39cc9902eabd08ff44
Alexander Lang <alex@upstre.am>
2013-04-11 18:11:48 +0200

    updates oauth endpoints

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cobot, Coworkers::Conf.app_id,
     Coworkers::Conf.app_secret, scope: 'navigation read',
       client_options: {site: Coworkers::Conf.app_site,
-        authorize_url: "#{Coworkers::Conf.site}/oauth2/authorize",
-        token_url: "#{Coworkers::Conf.site}/oauth2/access_token"}
+        authorize_url: "#{Coworkers::Conf.site}/oauth/authorize",
+        token_url: "#{Coworkers::Conf.site}/oauth/access_token"}
 end
