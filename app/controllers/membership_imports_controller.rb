15fadbf3a644c98313631daf692691c01e576786
Thilo Utke <thilo@upstre.am>
2016-03-04 18:03:54 +0100

    use faster view to get members to import, also don’t try to assign picture url as we infer this in the model

--- app/controllers/membership_imports_controller.rb
+++ app/controllers/membership_imports_controller.rb
@@ -4,7 +4,6 @@ class MembershipImportsController < ApplicationController
   def new
     existing_memberships = Membership.where(space_id: @space.id)
     @memberships = cobot_memberships.reject{|m|
-      m[:canceled_to].present? && Date.parse(m[:canceled_to]) < Date.today ||
         existing_memberships.map(&:cobot_id).include?(m[:id])
     }.map{|attributes|
       Membership.new(cobot_id: attributes[:id], name: attributes[:name].to_s)
@@ -17,7 +16,6 @@ class MembershipImportsController < ApplicationController
       ids.each do |id|
         membership_details = cobot_client.get(@space.subdomain, "/memberships/#{id}")
         @space.memberships.create cobot_id: membership_details[:id],
-          picture: membership_details[:picture],
           name: membership_details[:name]
       end
       flash[:notice] = 'Members successfully imported.'
@@ -32,6 +30,6 @@ class MembershipImportsController < ApplicationController
   end
 
   def cobot_memberships
-    cobot_client.get(@space.subdomain, '/memberships')
+    cobot_client.get(@space.subdomain, '/memberships', attributes: [:name, :id])
   end
 end
