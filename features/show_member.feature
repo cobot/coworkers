a177706a379aa7e12ea541b03f4718c11f2477cb
Alexander Lang <alex@upstre.am>
2016-05-31 11:49:25 +0200

    fixes specs

--- features/show_member.feature
+++ features/show_member.feature
@@ -4,7 +4,7 @@ Feature: Show member
 
   Scenario: show a coworker profile
     Given a space "co.up"
-      And "co.up" has a member "Joe Doe" with email "joedoe@cobot.me"
+      And "co.up" has a member "Joe Doe" with email "joedoe@cobot.me" and profession "rockstar programmer"
     When I visit the profile page of "joedoe@cobot.me"
     Then I should see "Joe Doe"
-      And I should see "General Information"
+      And I should see "rockstar programmer"
--- features/step_definitions/membership_steps.rb
+++ features/step_definitions/membership_steps.rb
@@ -3,6 +3,13 @@ Given(/^"([^"]*)" has a member "([^"]*)"(?: with email "([^"]*)")?$/) do |space_
   space_by_name(space_name).memberships.create!(name: member_name, user_id: user.id, cobot_id: member_name.gsub(/\W+/, '-'))
 end
 
+Given(/^"([^"]*)" has a member "([^"]*)" with email "([^"]*)" and profession "([^"]*)"$/) do |space_name, member_name, member_email, profession|
+  user = User.create!(email: member_email || 'joe@doe.com')
+  space_by_name(space_name).memberships.create!(profession: profession,
+    name: member_name, user_id: user.id, cobot_id: member_name.gsub(/\W+/, '-'))
+end
+
+
 Given(/^"([^"]*)" has a member "([^"]*)" with cobot id "([^"]*)"$/) do |space_name, member_name, cobot_id|
   user = User.create!(email: 'joe@doe.com', cobot_id: cobot_id)
   space_by_name(space_name).memberships.create!(name: member_name, user_id: user.id, cobot_id: member_name.gsub(/\W+/, '-'))
@@ -10,13 +17,13 @@ end
 
 Then(/^"([^"]+)" should be listed as a member of the space "([^"]+)"(?: once)?$/) do |membership_name, space_name|
   visit space_memberships_path(space_by_name(space_name))
-  expect(page.all('.member', text: membership_name).size).to eq(1)
+  expect(page.all('.test-member', text: membership_name).size).to eq(1)
 end
 
 Then(/^"([^"]+)" should not be listed as a member of the space "([^"]+)"(?: once)?$/) do |membership_name, space_name|
   visit space_memberships_path(space_by_name(space_name))
 
-  expect(page.all('.member', text: membership_name).size).to eq(1)
+  expect(page.all('.test-member', text: membership_name).size).to eq(1)
 end
 
 Then(/^I should see "([^"]*)" and "([^"]*)" as members of "([^"]*)"$/) do |name_1, name_2, space_name|
