21e75f38cc656c31a51999a0a1beac91b8c07741
Alexander Lang <alex@upstre.am>
2016-06-28 15:25:40 +0200

    fixes external links to be opened in new tab/window

--- app/helpers/memberships_helper.rb
+++ app/helpers/memberships_helper.rb
@@ -2,10 +2,10 @@ module MembershipsHelper
   def format_messenger(type, account)
     case type
     when 'Skype'
-      link_to account, "skype:#{account}?add"
+      link_to account, "skype:#{account}?add", target: '_blank'
     when 'Twitter'
       name = account.sub('@', '')
-      link_to "@#{name}", "http://twitter.com/#{name}"
+      link_to "@#{name}", "http://twitter.com/#{name}", target: '_blank'
     else
       account
     end
--- spec/helpers/memberships_helper_spec.rb
+++ spec/helpers/memberships_helper_spec.rb
@@ -2,18 +2,22 @@ require 'spec_helper'
 
 describe MembershipsHelper, '#format_messenger' do
   it 'turns a skype name into a skype url' do
-    expect(helper.format_messenger('Skype', 'test')).to eql('<a href="skype:test?add">test</a>')
+    expect(remove_target(helper.format_messenger('Skype', 'test'))).to eql('<a href="skype:test?add">test</a>')
   end
 
   it 'links to twitter' do
-    expect(helper.format_messenger('Twitter', 'test')).to eql('<a href="http://twitter.com/test">@test</a>')
+    expect(remove_target(helper.format_messenger('Twitter', 'test'))).to eql('<a href="http://twitter.com/test">@test</a>')
   end
 
   it 'deals with a twitter name that already has an @' do
-    expect(helper.format_messenger('Twitter', '@test')).to eql('<a href="http://twitter.com/test">@test</a>')
+    expect(remove_target(helper.format_messenger('Twitter', '@test'))).to eql('<a href="http://twitter.com/test">@test</a>')
   end
 
   it 'leaves jabber untouched' do
     expect(helper.format_messenger('Jabber', 'test')).to eql('test')
+  end
+
+  def remove_target(link)
+    link.sub(/ target="\S+"/, '')
   end
 end
